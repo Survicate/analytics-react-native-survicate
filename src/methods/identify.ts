043a5c5ee14d5327ed77d70621a54a544674b075
SurvicatePawelGodlewski <112856109+SurvicatePawelGodlewski@users.noreply.github.com>
2024-03-29 07:41:45 +0100

    Support for improved audience targeting, README update (#6)

--- src/methods/identify.ts
+++ src/methods/identify.ts
@@ -1,19 +1,32 @@
-import type { IdentifyEventType } from "@segment/analytics-react-native";
-import Survicate from "@survicate/react-native-survicate";
+import { IdentifyEventType, isString, isNumber, isBoolean } from "@segment/analytics-react-native";
+import Survicate, {UserTrait} from "@survicate/react-native-survicate";
 
 export default (event: IdentifyEventType) => {
   const userId = event.userId;
   const properties = event.traits;
 
   if (userId !== undefined) {
-    Survicate.setUserTrait("user_id", userId);
+    const userIdTrait = new UserTrait('user_id', userId);
+    Survicate.setUserTrait(userIdTrait);
   }
 
   for (const key in properties) {
     if (properties.hasOwnProperty(key)) {
       const value = properties[key];
-      if (typeof value === "string") {
-        Survicate.setUserTrait(key, value);
+      let trait;
+      switch (true) {
+        case isString(value):
+          trait = new UserTrait(key, value as string);
+          break;
+        case isNumber(value):
+          trait = new UserTrait(key, value as number);
+          break;
+        case isBoolean(value):
+          trait = new UserTrait(key, value as boolean);
+          break;
+      }
+      if (trait) {
+        Survicate.setUserTrait(trait);
       }
     }
   }
