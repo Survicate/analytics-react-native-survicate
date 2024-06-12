a957d916fa1e1ed02a14e237163181f9e27f02bc
SurvicatePawelGodlewski <112856109+SurvicatePawelGodlewski@users.noreply.github.com>
2024-06-12 08:58:23 +0200

    4.4.0 (#7)
    - Support for event properties

--- src/methods/track.ts
+++ src/methods/track.ts
@@ -2,5 +2,17 @@ import type { TrackEventType } from '@segment/analytics-react-native';
 import Survicate from '@survicate/react-native-survicate';
 
 export default async (event: TrackEventType) => {
-    Survicate.invokeEvent(event.event);
+    const properties: { [key: string]: string } = {};
+
+    for (const key in event.properties) {
+        if (event.properties.hasOwnProperty(key)) {
+            const value = event.properties[key];
+
+            if (typeof value === 'string') {
+                properties[key] = value;
+            }
+        }
+    }
+
+    Survicate.invokeEvent(event.event, properties);
 };
